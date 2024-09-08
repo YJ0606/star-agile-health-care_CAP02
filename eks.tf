ec12cf2ec10796dce675bdbd3c5997121e7f5f5a
Laxmikanta Giri <90712743+lax66@users.noreply.github.com>
2024-09-09 03:24:15 +0530

    Update eks.tf

--- eks.tf
+++ eks.tf
@@ -46,33 +46,3 @@ module "eks" {
 
   tags = local.tags
 }
-
-# Define variables
-variable "name" {
-  description = "Name of the cluster"
-  type        = string
-  default     = "my-cluster"
-}
-# Data source to fetch security group ID
-data "aws_security_group" "node_group" {
-  filter {
-    name   = "tag:aws:eks:cluster-name"
-    values = [var.name]
-  }
-
-  filter {
-    name   = "tag:kubernetes.io/cluster/${var.name}"
-    values = ["owned"]
-  }
-}
-
-# Security Group Rule to allow all traffic
-resource "aws_security_group_rule" "allow_all_traffic" {
-  type              = "ingress"
-  from_port         = 0
-  to_port           = 0
-  protocol          = "tcp"
-  security_group_id = data.aws_security_group.node_group.id
-
-  cidr_blocks = ["0.0.0.0/0"]
-}
